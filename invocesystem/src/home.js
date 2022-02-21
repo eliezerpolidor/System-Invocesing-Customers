cb4e323b0ba305b960b392c3baee19ee8f3808ff
DESKTOP-65OB39G\eliez <eliezerpolidor69@gmail.com>
2022-02-21 18:07:05 -0400

    update repository

--- invocesystem/src/home.js
+++ invocesystem/src/home.js
@@ -6,19 +6,18 @@ function Home() {
         div style = {
             { 'textAlign': 'left' } } >
         <
-        h2 > Home Page < /h2>  <
+        h2 > Home Page < /h2> <
         ul >
         <
         li >
-        My name is < b > Eliezer Polidor < /b>, Job in remotely how DEVELOPER WEB, live in Venezuela,  <
+        My name is < b > Eliezer Polidor < /b>, Job in remotely how DEVELOPER WEB, live in Venezuela,   <
         /li> <
         li >
         Specialize in HTML, CSS, JAVACRIPT REACTJS, NODEJS AND C#; my English level is basic <
-        /li> <
+        /li>  <
         li >
-        This is a sample App of use of this < b > React < /b> packages:  <
-        b > React Router v6: < /b> Client-side routing /
-        Redux. <
+        This is a sample App of use of this < b > React < /b> packages:   <
+        b > React Router v6: < /b> Client-side routing/Redux. <
         li > You can test the use of this package by clicking the <
         Link to = 'invoices'
         style = {
@@ -27,14 +26,16 @@ function Home() {
         style = {
             { color: 'red' } } > PURCHASE ORDER < /Link> on the Navbar.</li >
         <
-        /li>  <
-        li > Sorry this is an demo, some options this in Construction < /li> <
+        /li>   <
+        li >
+        Sorry this is an demo, some options this in Construction <
+        /li> <
         li > If want can download the program in my repository <
         a href = "https://github.com/eliezerpolidor/System-Invocesing-Customers.git"
         target = "_blank" > https: //github.com/eliezerpolidor/System-Invocesing-Customers.git</a> &nbsp; | &nbsp;
         <
-        /li> <
-        /ul> <
+        /li>  <
+        /ul>  <
         /div>
     );
 }
